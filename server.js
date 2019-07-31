46589bb510a0853e4e065922bc49405a77acd1ce
hasmukhbhai <darshit.hasmukhbhai1998@gmail.com>
2019-07-31 14:27:14 +0530

    dmmmm
    mmm

---
+++ server.js
@@ -0,0 +1,14 @@
+const express=require("express");
+const path=require("path");
+const app=express();
+if(process.env.NODE_ENV==='production')
+{
+	app.use(express.static(path.join(__dirname,'/build')));
+	app.get('*',(req,res,next)=>{
+		res.sendFile(path.join(__dirname + '/build/index.html'));
+	});
+}
+const port=process.env.PORT || 3002;
+app.listen(port,()=>{
+	console.log("port",port);
+});
