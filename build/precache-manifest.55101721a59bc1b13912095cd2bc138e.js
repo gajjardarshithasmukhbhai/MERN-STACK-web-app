6e135ca8424e2a59e956ce31a462918bdb310fc4
cismox owner <darshit.hasmukhbhai1998@gmail.com>
2019-08-09 15:40:36 +0530

    change successfully

---
+++ build/precache-manifest.55101721a59bc1b13912095cd2bc138e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4e0aeebb1ae5915791f486eca95b64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83c0fe3825965d175144",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "34602416301319193b2d",
+    "url": "/static/css/main.abb10204.chunk.css"
+  },
+  {
+    "revision": "83c0fe3825965d175144",
+    "url": "/static/js/2.50992f0c.chunk.js"
+  },
+  {
+    "revision": "34602416301319193b2d",
+    "url": "/static/js/main.b438d151.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
